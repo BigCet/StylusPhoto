5ab77ac8009350cdbdf76eb8873a6ab46e92a028
BigCet <38659642+BigCet@users.noreply.github.com>
2021-02-28 13:22:00 +0100

    Context processor használata

--- pages/views.py
+++ pages/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-def home_view(reqvest):
 
-    return render(reqvest, "home.html")
+
+def home_view(reqvest):
+    context = {
+        "names": ["Ákos", "Robert", "Csaba", "Tamás"]
+    }
+
+    return render(reqvest, "home.html", context)
+
 
 def contact_view(reqvest):
     return render(reqvest, "contact.html")
 
+
 def about_view(reqvest):
     return render(reqvest, "about.html")
+
 
 def gallery_view(reqvest):
     return render(reqvest, "gallery.html")
