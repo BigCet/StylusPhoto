15e58340b142c3c0f3eb1e6f0aeb00c9baaa9dea
BigCet <38659642+BigCet@users.noreply.github.com>
2021-03-26 19:04:09 +0100

    Frontend ok.

--- pages/views.py
+++ pages/views.py
@@ -1,23 +1,42 @@
 from django.shortcuts import render, HttpResponse
+from utilities.mock_data import images_generator
 
-# Create your views here.
 
-
-def home_view(reqvest):
+def home_view(request):
+    # todo create a model for this
     context = {
-        "names": ["Ákos", "Robert", "Csaba", "Tamás"]
+        "titles": "photographica",
+        "subtitles": "Ahol a fotó és a grafika találkozik",
+        "content": "Grafikus vagyok, és fotós. Kreatív képeimmel szeretném azt megmutatni, ami nem létezik (vagy mégis?) Célom, hogy az emberek megálljanak képeim előtt és elgondolkozzanak. Érezzenek valamit, csodálkozzanak, szeressék, gyűlöljék, de mindenképp érzéseket váltson ki. Ne egyszerűen csak elmenjenek előtte, továbblépjenek. És ha valaki visszatér, hogy újra megnézze, akkor már boldog vagyok.",
+        "photos": images_generator(5)
+
     }
 
-    return render(reqvest, "home.html", context)
+    return render(request, "home.html", context)
+
+def about_view(request):
+    # todo create a model for this
+    context = {
+        "title": "Hogyan is készülnek a képeim?",
+        "subtitle": "",
+        "content": "Előszőr is tervezéssel! Sokat rajzolok, vázlatokat készítek a fotózások előtt. Egyrészt, szeretek koncepcióval készülni, és szeretnék az ügyfélhez, modellhez legjobban illő ötletekkel előállni. A fotózásra emiatt felkészülten érkezek, és lehet is a koncepció kivitelezésével foglalkozni.",
+        "photos": images_generator(1)
+
+    }
+    return render(request, "about.html", context)
+
+def contact_view(request):
+    # todo create a model for this
+    context = {
+        "title": "Kérjen tőlünk ajánlatot!",
+        "subtitle": "",
+        "content": "",
+        "photos": images_generator(1)
+    }
+    return render(request, "contact.html", context)
 
 
-def contact_view(reqvest):
-    return render(reqvest, "contact.html")
 
 
-def about_view(reqvest):
-    return render(reqvest, "about.html")
 
 
-def gallery_view(reqvest):
-    return render(reqvest, "gallery.html")
--- stylusPhoto/urls.py
+++ stylusPhoto/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = {
     path('admin/', admin.site.urls),
 
-    path('', include('pages.urls'))
+    path('', include('pages.urls')),
+    path('gallery/', include('gallery.urls'))
 }
